e14120794f66bd7a09845e8d80f6e7faccf5fe66
Michał <michaljanpatyk@gmail.com>
2019-05-07 22:22:14 +0200

    Add configuration files, extract method, change the way of checking arguments

--- main.cpp
+++ main.cpp
@@ -3,32 +3,43 @@
 #include <string>
 #include "list.h"
 
-#define NDEBUG
-
-void fromFileToList(double min, double max, std::istream &file, List *list);
+//#define NDEBUG
 
 #include <cassert>
+
+void fromFileToList(double min, double max, std::istream &basicIstream, List *list);
 
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
     string filename = argv[1];
-    double min = atof(argv[2]);
-    double max = atof(argv[3]);
-    string str;
+    char *p;
+    double min = strtod(argv[2], &p);
+    assert(*p == 0);
+    if (*p) {// conversion failed because the input wasn't a number
+        cout << "Podany zakres jest nieprawidłowy, kończę działanie!";
+        return 1;
+    }
+    assert(*p == 0);
+    double max = strtod(argv[3], &p);
+
+    if (*p) {// conversion failed because the input wasn't a number
+        cout << "Podany zakres jest nieprawidłowy, kończę działanie!";
+        return 1;
+    }
     auto fb = new filebuf();
     fb->open(filename, ios::in);
     assert(fb->is_open());
     if (!fb->is_open()) {
-        cout << "Nie udało się otworzyć wskazanego pliku, kończę działanie";
+        cout << "Nie udało się otworzyć wskazanego pliku, kończę działanie!";
         return 1;
     }
-    istream file(fb);
+    istream basicIstream(fb);
     List *list = new List();
     assert(list != nullptr);
 
-    fromFileToList(min, max, file, list);
+    fromFileToList(min, max, basicIstream, list);
 
     fb->close();
     list->showRightToLeft();
@@ -37,11 +48,12 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void fromFileToList(double min, double max, istream &file, List *list) {
+void fromFileToList(double min, double max, istream &basicIstream, List *list) {
     double value;
-    while (!file.eof()) {
-        if (isdigit(file.peek())) {
-            file >> value;
+    while (!basicIstream.eof()) {
+        assert(isdigit(basicIstream.peek()));
+        if (isdigit(basicIstream.peek())) {
+            basicIstream >> value;
             assert(value >= min);
             assert(value <= max);
             if (value >= min && value <= max)
@@ -49,7 +61,7 @@ void fromFileToList(double min, double max, istream &file, List *list) {
             else cout << "poza zakresem" << endl;
         } else {
             cout << "nie liczba" << endl;
-            file.ignore();
+            basicIstream.ignore();
         }
     }
 }
